43a8bbd91dc64cd963ea6346001d8d7f21732546
rina132702 <charina1327@gmail.com>
2026-09-05 01:48:01 -0700

    Delete WDD131 directory

--- WDD131/scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-const currentYear = document.querySelector("#currentyear");
-const today = new Date();
-
-currentYear.textContent = today.getFullYear();
-
-const lastModified = document.querySelector("#lastModified");
-lastModified.textContent = `Last Modification: ${document.lastModified}`;
